6f0c586cc3544cb5743dca99a3a8aaab5b93e897
Jayandra Hamal <salonhamal278@gmail.com>
2024-10-11 21:35:20 +0545

    generate JWT

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,53 +1,55 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs"
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signup = async (req, res) => {
   try {
     const { fullName, username, password, confirmPassword, gender } = req.body;
-  
-    if (!fullName ||!username ||!password ||!confirmPassword ||!gender) {
+
+    if (!fullName || !username || !password || !confirmPassword || !gender) {
       return res.status(400).json({ message: 'All fields are required' });
     }
-  
-    if(password !== confirmPassword) {
+
+    if (password !== confirmPassword) {
       return res.status(400).json({ message: 'Passwords do not match' });
     }
-  
-    const user = await User.findOne({username})
-    if(user) {
+
+    const user = await User.findOne({ username })
+    if (user) {
       return res.status(400).json({ message: 'Username already exists' });
     }
-  
+
     //HASH PASSWORD HERE
     const salt = await bcrypt.genSalt(10)
     const hashedPassword = await bcrypt.hash(password, salt)
-    
-  
+
+
     const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
     const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
-  
+
     const newUser = new User({
       fullName,
       username,
       password: hashedPassword,
       gender,
-      profilePic: gender ==='male'? boyProfilePic : girlProfilePic
+      profilePic: gender === 'male' ? boyProfilePic : girlProfilePic
     })
 
-    if(newUser){
+    if (newUser) {
       //Generate JWT token here
+      generateTokenAndSetCookie(newUser._id, res)
       await newUser.save()
-  
+
       res.status(201).json({
         _id: newUser._id,
         fullName: newUser.fullName,
         username: newUser.username,
         profilePic: newUser.profilePic
-       });
+      });
     } else {
       res.status(400).json({ message: 'Failed to create user' });
     }
-  
+
   } catch (error) {
     console.log("Error in signup controller", error.message)
     res.status(500).json({ message: 'Server Error' });
