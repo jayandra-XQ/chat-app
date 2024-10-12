817364fa810c70b5880aa9429bf87b098c885d58
Jayandra Hamal <salonhamal278@gmail.com>
2024-10-12 17:48:28 +0545

    get messages endpoint

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -39,3 +39,25 @@ export const sendMessage = async (req,res) => {
     res.status(500).json({ message: 'Server Error' });
   }
 }
+
+export const getMessages = async (req,res) => {
+  try {
+
+    const {id: userToChatId} = req.params;
+    const senderId = req.user._id;
+
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, userToChatId] }
+    }).populate("messages")
+
+    if (!conversation) {
+      return res.status(404).json({ message: 'No conversation found' });
+    }
+
+    const messages = conversation.messages;
+    res.status(200).json(messages)
+  } catch (error) {
+    console.error("Error in get messages controller", error.message)
+    res.status(500).json({ error: 'Server Error' });
+  }
+}
