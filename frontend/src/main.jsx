9bc884939d894be18b88c42be5ecae31aeda76e1
Jayandra Hamal <salonhamal278@gmail.com>
2024-10-16 22:31:04 +0545

    create AuthContext functionality

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,14 @@ import App from './App.jsx'
 import './index.css'
 
 import { BrowserRouter } from 'react-router-dom'
+import { AuthContextProvider } from './context/AuthContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+    <AuthContextProvider>
       <App />
+    </AuthContextProvider>
     </BrowserRouter>
   </StrictMode>,
 )
