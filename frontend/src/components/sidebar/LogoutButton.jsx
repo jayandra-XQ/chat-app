bb1a166cab31c8c5794d324207cf1f2cd45b905a
Jayandra Hamal <salonhamal278@gmail.com>
2024-10-16 22:50:45 +0545

    Logout functionality

--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -1,9 +1,19 @@
 import { IoIosLogOut } from "react-icons/io";
 
+import useLogout from "../../hooks/useLogout";
+
 const LogoutButton = () => {
+
+  const { logout} = useLogout()
+
+  const handleLogout = async () => {
+    await logout()
+  }
   return (
     <div className="mt-auto">
-      <IoIosLogOut className="w-6 h-6 text-white cursor-pointer"/>
+      <IoIosLogOut className="w-6 h-6 text-white cursor-pointer"
+      onClick={handleLogout}
+      />
     </div>
   )
 }
