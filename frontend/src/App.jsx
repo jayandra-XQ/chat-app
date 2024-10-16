5db19c6fc1ec2044d545bf278f79e8028bffdfe7
Jayandra Hamal <salonhamal278@gmail.com>
2024-10-16 21:54:04 +0545

    signUp Functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
 import Login from "./pages/login/Login"
 import SignUp from "./pages/signup/SignUp"
 import Home from "./pages/home/Home"
+import { Route, Routes } from "react-router-dom"
+import  { Toaster } from 'react-hot-toast';
+
 
 const App = () => {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-       <Home /> 
-     
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+      <Toaster />
     </div>
   )
 }
