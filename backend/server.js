73de1fbf395b7f26b531bda4321dee4ff96364b6
Jayandra Hamal <salonhamal278@gmail.com>
2024-10-21 18:41:41 +0545

    deployment code

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path"
 import express from "express"
 import 'dotenv/config'
 import cookieParser from "cookie-parser"
@@ -13,13 +14,21 @@ import { app, server } from "./socket/socket.js"
 
 const PORT = process.env.port || 5000;
 
+const __dirname = path.resolve();
+
 
 app.use(express.json())
 app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
-app.use("/api/users", userRoutes)
+app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+app.get("*", (req,res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 
 server.listen(PORT, () => {
